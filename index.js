9ae0e98bc38effb2834f78759cc6968dc543e4af
Jess Pesale <jess_pesale@aol.com>
2021-09-12 17:48:13 -0400

    added new workout form

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ function getExerciseAttributes() {
     fetch(attr_url)
     .then(response => response.json())
     .then(details => {
-        // let det = details
-        // debugger
-        // console.log(details)
 
     details.data.forEach(detail => {
         const detailsSetUp = `<div data-id=${detail.id}>
@@ -23,7 +20,7 @@ function getExerciseAttributes() {
           </div>
           <br><br>`;
 
-          document.querySelector('#detail-holder').innerHTML += detailsSetUp
+          document.querySelector('#detail-container').innerHTML += detailsSetUp
       })
     })
 }
