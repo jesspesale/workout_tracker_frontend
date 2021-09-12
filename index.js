30e5805f1eb39177450f6d120915eaef13ee3e44
Jess Pesale <jess_pesale@aol.com>
2021-09-12 15:48:53 -0400

    fetted attributes/ workout title to DOM in a div

--- index.js
+++ index.js
@@ -8,8 +8,24 @@ document.addEventListener('DOMContentLoaded', () => {
 function getExerciseAttributes() {
     fetch(attr_url)
     .then(response => response.json())
-    .then(attr => {
-        console.log(attr)
+    .then(details => {
+        // let det = details
+        // debugger
+        // console.log(details)
+
+    details.data.forEach(detail => {
+        const detailsSetUp = `<div data-id=${detail.id}>
+            <h2>${detail.attributes.workout.title}</h2>
+            <h3>Category: ${detail.attributes.category}</h3>
+            <h4>Date: ${detail.attributes.date}</h4>
+            <h4>Duration: ${detail.attributes.category.name} (in minutes)</h4>
+            <button data-id=${detail.id}>edit</button>
+          </div>
+          <br><br>`;
+
+          document.querySelector('#detail-holder').innerHTML += detailsSetUp
+      })
     })
 }
+
 
