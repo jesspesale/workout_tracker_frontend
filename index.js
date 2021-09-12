bf3556cb6adf6942ed9a80d08dd2310a460754ec
Jess Pesale <jess_pesale@aol.com>
2021-09-12 15:29:18 -0400

    started exercise attributes fetch, got to load the json in the console

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("index.js is working")
+const attr_url = "http://127.0.0.1:3000/api/v1/exercise_attributes"
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Dom is loaded")
+    getExerciseAttributes()
+} )
+
+function getExerciseAttributes() {
+    fetch(attr_url)
+    .then(response => response.json())
+    .then(attr => {
+        console.log(attr)
+    })
+}
+
