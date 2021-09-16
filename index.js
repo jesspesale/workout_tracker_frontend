f2af6d5d141b0230619842ad6b06ec401e263825
Jess Pesale <jess_pesale@aol.com>
2021-09-15 21:42:32 -0400

    fixed code to finally work with a post fetch for creating a new workout with exercise attributes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const attr_url = "http://127.0.0.1:3000/api/v1/exercise_attributes"
+// Creating a new workout and its atributes associated already
+
+const workout_url = "http://127.0.0.1:3000/api/v1/workouts"
 
 document.addEventListener('DOMContentLoaded', () => {
-    getExerciseAttributes()
+    getWorkouts()
     const newWorkoutForm = document.querySelector("#new-workout-form")
 
     newWorkoutForm.addEventListener("submit", (e) =>{
@@ -9,73 +11,75 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 } )
 
-function getExerciseAttributes() {
-    fetch(attr_url)
+function getWorkouts() {
+    fetch(workout_url)
     .then(response => response.json())
-    .then(details => {
-            //  console.log(details)
-        details.data.forEach(detail => {
-        const detailsSetUp = `<div data-id=${detail.id}>
-            <h2>${detail.attributes.workout.title}</h2>
-            <h3>Date: ${detail.attributes.date}</h3>
-            <h3>Category: ${detail.attributes.category}</h3>
-            <h4>Calories: ${detail.attributes.calories}</h4>
-            <h4>Duration: ${detail.attributes.duration} (in minutes)</h4>
-            <button data-id=${detail.id}>edit</button>
-          </div>
-          <br><br>`;
+    .then(workouts => {
+        // console.log(workouts)
+        workouts.data.forEach(w => {
+            let workout = w.attributes
 
-          document.querySelector('#attributes-container').innerHTML += detailsSetUp
+            workout.exercise_attributes.forEach(attr => {
+                let attributes = attr
+                 const postAttributes = ` <div data-id=${w.id}>
+                 <h2>Title: ${workout.title}</h2>
+                 <h3>Date: ${workout.date}</h3>
+                <h3>Category: ${attributes.category}</h3>
+                <h4>Calories: ${attributes.calories}</h4>
+                <h4>Duration: ${attributes.duration} (in minutes)</h4> 
+                <button data-id=${workout.id}>Add another exercise</button>
+                </div>
+                <br><br>`
+
+                 document.querySelector('#workout-container').innerHTML +=  postAttributes
+            })
+
       })
     })
 }
+//  find id of whatever button is pushed
+//  send back workout of that id
+//  
 
 function createFormHandler(e) {
-    e.preventDefault()  // prevents the page from refreshing when submit is clicked      
-    // let even = e
-    // console.log(e)
-    // debugger
+    e.preventDefault()          // prevents the page from refreshing when submit is clicked      
     const titleInput = document.querySelector("#input-title").value
+    const dateInput = document.querySelector("#input-date").value
     const categoryChoice = document.querySelector("#category").value
     const calorieInput = document.querySelector("#input-calories").value
-    const dateInput = document.querySelector("#input-date").value
     const durationInput = document.querySelector("#input-duration").value
-    // const workoutId = 
-    // workout ID??
-    postWorkout(titleInput, categoryChoice, calorieInput, dateInput, durationInput)
-
+    postWorkout(titleInput, dateInput, categoryChoice, calorieInput,  durationInput)
 }
 
-
-function postWorkout(title, category, calories, date, duration) {
-    console.log(title, category, calories, date, duration)
-    let bodyData = {title, category, calories, date, duration}
-    fetch(attr_url, {
+function postWorkout(title, date, category, calories, duration) {
+    // console.log(title, date, category, calories, duration)
+    let bodyData = {title, date, category, calories, duration}
+    fetch(workout_url, {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(bodyData)
-        // JSON.stringify({        
-        //     title: title,
-        //     category: category,
-        //     date: date,
-        //     duration: duration
-        // }  )   
     })
     .then(response => response.json())
-    .then(workout => {
-        console.log(workout)
+    .then(wo => {
+        let workout = wo.data
+        // console.log(workout)
+        let w = workout.attributes.exercise_attributes[workout.attributes.exercise_attributes.length - 1]
         // debugger
         const newWorkout = `
             <div data-id=${workout.id}>
-            <h2>${workout.title}</h2>
-            <h3>Date: ${workout.date}</h3>
-            <h3>Category: ${workout.category}</h3>
-            <h4>Calories: ${workout.calories}</h4>
-            <h4>Duration: ${workout.duration} (in minutes)</h4>
-            <button data-id=${workout.id}>edit</button>
+            <h2>${workout.attributes.title}</h2>
+            <h3>Date: ${workout.attributes.date}</h3>
+            <h3>Category: ${w.category}</h3>
+            <h4>Calories: ${w.calories}</h4>
+            <h4>Duration: ${w.duration} (in minutes)</h4>
+            <button data-id=${w.id}>Add another exercise</button>
             </div>
         `
         document.querySelector('#attributes-container').innerHTML += newWorkout
-        // can refactor ^ into a global variable since used more than once
+    //     // can refactor ^ into a global variable since used more than once
     })
 }
+
+
+
+
