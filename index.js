858c3b85b4408090f691ee2618af85ea5d2779b6
Jess Pesale <jess_pesale@aol.com>
2021-09-17 09:23:38 -0400

    added render function to put workouts to the DOM from fetch and post request

--- index.js
+++ index.js
@@ -18,37 +18,32 @@ function getWorkouts() {
         // console.log(workouts)
         workouts.data.forEach(w => {
             let workout = w.attributes
-
+            let workoutId = w.id
+            // console.log(w)
+            // console.log(workout)
             workout.exercise_attributes.forEach(attr => {
-                let attributes = attr
-                 const postAttributes = ` <div data-id=${w.id}>
-                 <h2>Title: ${workout.title}</h2>
-                 <h3>Date: ${workout.date}</h3>
-                <h3>Category: ${attributes.category}</h3>
-                <h4>Calories: ${attributes.calories}</h4>
-                <h4>Duration: ${attributes.duration} (in minutes)</h4> 
-                <button data-id=${workout.id}>Add another exercise</button>
-                </div>
-                <br><br>`
+                
+                // let attributes = attr
+                render(workoutId, workout, attr )
 
-                 document.querySelector('#workout-container').innerHTML +=  postAttributes
             })
 
       })
     })
 }
-//  find id of whatever button is pushed
-//  send back workout of that id
-//  
 
-function createFormHandler(e) {
-    e.preventDefault()          // prevents the page from refreshing when submit is clicked      
-    const titleInput = document.querySelector("#input-title").value
-    const dateInput = document.querySelector("#input-date").value
-    const categoryChoice = document.querySelector("#category").value
-    const calorieInput = document.querySelector("#input-calories").value
-    const durationInput = document.querySelector("#input-duration").value
-    postWorkout(titleInput, dateInput, categoryChoice, calorieInput,  durationInput)
+function render(workoutId, workout, attributes) {
+        const postAttributes = ` <div data-id=${workoutId}>
+        <h2>Title: ${workout.title}</h2>
+        <h3>Date: ${workout.date}</h3>
+        <h3>Category: ${attributes.category}</h3>
+        <h4>Calories: ${attributes.calories}</h4>
+        <h4>Duration: ${attributes.duration} (in minutes)</h4> 
+        <button data-id=${workout.id}>Add another exercise</button>
+        </div>
+        <br><br>`
+
+        document.querySelector('#workout-container').innerHTML +=  postAttributes
 }
 
 function postWorkout(title, date, category, calories, duration) {
@@ -62,24 +57,26 @@ function postWorkout(title, date, category, calories, duration) {
     .then(response => response.json())
     .then(wo => {
         let workout = wo.data
+        let workoutId = workout.id
         // console.log(workout)
         let w = workout.attributes.exercise_attributes[workout.attributes.exercise_attributes.length - 1]
         // debugger
-        const newWorkout = `
-            <div data-id=${workout.id}>
-            <h2>${workout.attributes.title}</h2>
-            <h3>Date: ${workout.attributes.date}</h3>
-            <h3>Category: ${w.category}</h3>
-            <h4>Calories: ${w.calories}</h4>
-            <h4>Duration: ${w.duration} (in minutes)</h4>
-            <button data-id=${w.id}>Add another exercise</button>
-            </div>
-        `
-        document.querySelector('#new-workout-container').innerHTML += newWorkout
-    //     // can refactor ^ into a global variable since used more than once
+        render(workoutId, workout.attributes, w)
+        console.log(workout)
+
     })
 }
 
 
+
+function createFormHandler(e) {
+    e.preventDefault()          // prevents the page from refreshing when submit is clicked      
+    const titleInput = document.querySelector("#input-title").value
+    const dateInput = document.querySelector("#input-date").value
+    const categoryChoice = document.querySelector("#category").value
+    const calorieInput = document.querySelector("#input-calories").value
+    const durationInput = document.querySelector("#input-duration").value
+    postWorkout(titleInput, dateInput, categoryChoice, calorieInput,  durationInput)
+}
 
 
