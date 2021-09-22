922b0995f2639739261f98e9a758e9004d16cf1c
Jess Pesale <jess_pesale@aol.com>
2021-09-22 19:33:18 -0400

    added code for deleting a exercise attribute

--- src/exercise_attributes.js
+++ src/exercise_attributes.js
@@ -3,6 +3,7 @@
 class ExerciseAttribute {
 
     constructor(attribute) {
+        // debugger
         this.id = attribute.id
         this.category = attribute.category
         this.calories = attribute.calories
@@ -11,13 +12,50 @@ class ExerciseAttribute {
         ExerciseAttribute.all.push(this)
     }
 
-    renderExercise() {
+    // static findById(id) {
+    //     this.all.find(exercise_attribute => exercise_attribute.id === this.id)
+    // }
 
+    attachDeleteButtonListener() {
+       
+    const deleteExerciseButton = document.getElementById(`button-${this.id}`)
+   // debugger
+    
+        deleteExerciseButton.addEventListener('click', (e) => {
+            this.deleteExercise(e)
+        }
+        )
+    }
+
+    //static deleteExercise(e) {
+     deleteExercise(e) {
+         const deleteExercise = document.getElementById(`exercise-${this.id}`)
+         let ex = e
+        e.preventDefault()
+        const exercise = {id: this.id}
+        //  debugger
+        return fetch( exercise_url + "/" + this.id, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+             },
+             body: JSON.stringify(exercise)
+        })
+        .then(resp => resp.json())
+        .then(data => deleteExercise.remove() )
+
+    }
+
+
+
+    renderExercise() {
         return `
+        <div id="exercise-${this.id}">
         <h3>Category: ${this.category}</h3>
         <h4>Calories: ${this.calories}</h4>
         <h4>Duration: ${this.duration} (in minutes)</h4> 
-        <button data-id=${this.workout_id}>Delete Workout</button>
+        <button id="button-${this.id}">Delete Exercise</button>
+        </div>
         <br><br><br>`
     }
 
